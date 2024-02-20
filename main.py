82a25462e2324900935874acc7ff850facf0977b
Aurelio Correia da Silva <aureliocorreia1991br@gmail.com>
2024-02-20 17:53:19 -0300

    mudança do framework flask para o FastAPI

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-from flask import Flask
+from fastapi import FastAPI
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route("/")
-def  inicio():
-    return "<h1>Pagina Incial</h1>"
+@app.get("/")
+def home():
+    return "Página inicial"
 
-@app.route("/falecom")
-def  fale_conosco():
-    return "<h1>fale conosco</h1>"
-
-@app.route("/sobre")
-def  sobre():
-    return "<h1>Sobre nossa ideia</h1>"
-
-if "__main__" == __name__:
-    app.run()
+@app.get("/contato")
+def contato():
+    return "Página de contato"
